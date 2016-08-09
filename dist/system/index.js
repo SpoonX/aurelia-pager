6091e9a56ba3eaca58db89792a32f3be628c3c82
doktordirk <doktordirk@outlook.de>
2016-08-09 08:12:17 +0200

    chore(project): new build

--- dist/system/index.js
+++ dist/system/index.js
@@ -8,7 +8,7 @@ System.register(['./aurelia-pager'], function (_export, _context) {
       var _exportObj = {};
 
       for (var _key in _aureliaPager) {
-        if (_key !== "default") _exportObj[_key] = _aureliaPager[_key];
+        if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _aureliaPager[_key];
       }
 
       _export(_exportObj);
