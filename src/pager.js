0903913b7cadd529afe2c9d06ef60f9a63f958be
louislewis2 <Louis@nutcracker.co.za>
2017-08-01 16:00:10 +0200

    feat(pager): add optional page change event
    add an optional page change event, which will be called when the page value changes. this will simplify use cases where the pager is used stand alone or when server side paging is required. update readme to reflect the bindable property

--- src/pager.js
+++ src/pager.js
@@ -14,6 +14,7 @@ export class Pager {
   @bindable criteria      = {}; // search criteria for DB resource
   @bindable resource;           // data resource, either a ORM or a array
   @bindable pages;              // total amount of pages
+  @bindable onPageChanged;      // optional event to call when page value changes
 
   attached() {
     if (!this.page) {
@@ -38,6 +39,9 @@ export class Pager {
   pageChanged(newValue, oldValue) {
     if (newValue !== oldValue) {
       this.goToPage(newValue);
+	  if (this.onPageChanged !== null) {
+        this.onPageChanged(newValue, oldValue);
+      }
     }
   }
 
